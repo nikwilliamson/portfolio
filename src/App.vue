853809c0d9833cee588d6946ca6838664f65ee68
Nik Williamson <nikwilliamson@niks-mbp-2.lan>
2022-09-22 23:47:39 -0400

    Redid background for mobile

--- src/App.vue
+++ src/App.vue
@@ -30,15 +30,26 @@ export default {
     this.positions = await this.getPositions();
 
     const root = document.documentElement;
- 
+    
     document.addEventListener('mousemove', evt => {
+      
         let x = evt.clientX / innerWidth;
         let y = evt.clientY / innerHeight;
-    
+        
         root.style.setProperty('--mouse-x', x);
         root.style.setProperty('--mouse-y', y);
     });
+
+    document.addEventListener('dragover', evt => {
+      
+      let x = evt.clientX / innerWidth;
+      let y = evt.clientY / innerHeight;
+      
+      root.style.setProperty('--mouse-x', x);
+      root.style.setProperty('--mouse-y', y);
+  });
   },
+
   
   methods: {
     getPositions: async () => {

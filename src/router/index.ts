0bfc5e94bc9e41fdd4a5508f41fe9f2a07255f19
Nik Williamson <p1571024@c02dx83kmd6r-p.lan>
2023-12-22 13:12:21 -0500

    fixed page title and package

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,7 +7,8 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      title: 'Home'
     },
     {
       path: '/about',

1978841da52e15bd035edbdcb70db79b827a5148
Nik Williamson <nikwilliamson@niks-mbp-2.lan>
2024-04-05 15:44:08 -0400

    Added Google Analytics

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import VueGtag from "vue-gtag";
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
-app.mount('#app')
+app.use(VueGtag, {
+  config: { id: "G-JV19FZ8F7V" }
+}).mount("#app");
